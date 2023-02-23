abf23efc30b7a922677f27dcfe46338fafa6a28c
MATEUS LIMA E SILVA <mateus_78_@hotmail.com>
2023-02-23 13:31:54 -0300

    Demostração Aula 04.

---
+++ Aula02_HeloWord/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Aula02_HeloWord
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var valor = int.Parse(Console.ReadLine());
+            Console.WriteLine("valor digitado foi:" + valor);
+            Console.ReadLine();
+        }
+    }
+}
