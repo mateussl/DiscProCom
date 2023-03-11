b30329ea11c9fa95aa2b3958e16fc2cd740f25d1
MATEUS LIMA E SILVA <mateus_78_@hotmail.com>
2023-03-10 21:35:21 -0300

    Aula 04

---
+++ Aula03_Variaveis/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Aula03_Variaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            string nome = "Mateus";
+            idade = 10;
+            DateTime dataN = DateTime.Now;
+            DateTime? dataMatricula = null;
+            const string token = "xpo";
+
+            
+            Pessoa pessoaAluno = new Pessoa("Fulano de Tal",26);
+            
+
+            Console.WriteLine("Pessoa Nome "+ pessoaAluno.Nome);
+            Console.WriteLine("Pessoa Idade " + pessoaAluno.Idade);
+
+
+        }
+
+    }
+
+    public class Pessoa 
+    {
+        public string Nome { get; private set; }
+        public int Idade { get; set; }
+        public int AnoN { get; set; }
+        public DateTime? DataMatricula { get; set; }
+
+        public Pessoa(string nome,int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+            CalcularAnoN();
+
+        }
+
+        private void CalcularAnoN()
+        {
+            int anoAtual= DateTime.Now.Year;
+            AnoN= anoAtual-Idade;
+        }
+       
+    }
+}
---
+++ Aula04_ExprAritLogic/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Aula04_ExprAritLogic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Qual seu Nome?");
+            var nome = Console.ReadLine();
+            Console.WriteLine("Qual sua Idade?");
+           
+            try
+            {
+                int idade = int.Parse(Console.ReadLine());
+                if(idade>=30) { throw new Exception("A idade deverá ser menor que 30"); }
+                Console.WriteLine($"Seu nome é {nome} sua idade é: " + idade);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Você Digitou o valor errado!!! Motivo: {ex.Message}");
+            }
+
+            //Exemplo 2
+            Console.WriteLine("Exemplo 02");
+            Pessoa Fulano = new Pessoa("Fulano de Tal", 35);
+            Pessoa Sicrano = new Pessoa("Sicrano de Tal", 24);
+            Pessoa Beltrano = new Pessoa("Beltrado de Tal", 43);
+
+            Pessoa[] pessoas = new Pessoa[] { Fulano, Sicrano, Beltrano };
+           
+            foreach(var pessoa in pessoas) {
+                Console.WriteLine(pessoa.ToString());
+                if (pessoa.Idade == 24)
+                {
+                    Console.WriteLine("Pessoa Encontrada no laço "+pessoa.ToString());
+                }
+            }
+            
+
+            
+
+        }
+        public class Pessoa
+        {
+            public Pessoa(string nome, int idade)
+            {
+                Nome = nome;
+                Idade = idade;
+            }
+
+            public string Nome { get; set; }
+
+            public int Idade { get; set; }
+
+            public override string ToString()
+            {
+                return $"Nome: {Nome} e idade {Idade}.";
+            }
+
+        }
+    }
+}
