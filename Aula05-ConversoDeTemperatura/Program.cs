ecc79a57d8d0c11e7f38d0e16406ff6efc404e3c
MATEUS LIMA E SILVA <mateus_78_@hotmail.com>
2023-03-14 09:00:30 -0300

    Aula 05

---
+++ Aula05-ConversoDeTemperatura/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Aula05_ConversoDeTemperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //dotnet run
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.WriteLine("Informe qual a temperatura");
+                float temperatura = float.Parse(Console.ReadLine());
+                Console.WriteLine("Digite C para converter em Celsius ou F para Fahrenheit ");
+                string opcao = Console.ReadLine().ToLower().Substring(0, 1);
+
+                if (opcao == "f")
+                {
+                    var result = temperatura * 1.8 + 32;
+                    Console.WriteLine($"{result}F");
+                }
+                else if (opcao == "c")
+                {
+                    var result = (temperatura - 32) / 1.8;
+                    Console.WriteLine($"{result}C");
+                }
+                else
+                {
+                    Console.WriteLine("Nenhuma opção selecionada");
+                }
+                Console.WriteLine("Deseja realizar outra conversão S ou N ?");
+                var executarOpcao = Console.ReadLine().ToLower().Substring(0, 1);
+                continuar = executarOpcao=="s" ? true :false;
+
+            }
+        }
+    }
+}
